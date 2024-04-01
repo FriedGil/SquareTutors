1f6ad3790449b46a6cc1f46f83cffb7952a1807e
Gil <friedmang@bxscience.edu>
2024-04-01 18:21:12 -0400

    sizing fix part 2

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -10,14 +10,14 @@
 
 
 <div class="navbar bg-base-100">
-	<div class="flex-1">
+	<div class="flex-1 hidden sm:block">
 		<a class="btn btn-ghost text-xl" href="/">▢ Square Tutors</a>
 	</div>
 	<div class="flex-2">
 		<ul class="menu menu-horizontal px-1">
-		<li><a href="/about" class=" hidden md:block text-sm sm:text-md md:text-lg">About</a></li>
+		<li><a href="/about" class="text-sm sm:text-md md:text-lg">About</a></li>
 		<li><a href="/services" class="text-sm sm:text-md md:text-lg">Services</a></li>
-		<li><a href="/subjects" class="hidden md:block text-sm sm:text-md md:text-lg">Subjects</a></li>
+		<li><a href="/subjects" class=" text-sm sm:text-md md:text-lg">Subjects</a></li>
 		<li><a href="/contact" class="text-sm sm:text-md md:text-lg">Contact</a></li>
 
 		</ul>
